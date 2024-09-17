6fcb180f2842e916eb3ea9dcf3d7f194eaa50659
YakataR9 <116081382+YakataR9@users.noreply.github.com>
2024-09-17 14:54:07 +0200

    Add files via upload

---
+++ 3oszthato2/3oszthato2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3oszthato2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adjon meg egy számot(1-20-ig): ");
+            int szam = Convert.ToInt32(Console.ReadKey());
+            if (szam % 3 == 0 ) 
+            {
+                Console.WriteLine("A szám osztható 3-mal");
+            }
+            else
+            {
+                Console.WriteLine("A szám nem osztható 3-mal");
+            }
+            Console.ReadKey();                
+        }
+    }
+}
